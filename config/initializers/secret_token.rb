5b0b6f133b85ad8c56426774d97b560ab95ff0a9
Kyle Smyth <kylesmyth@gmail.com>
2010-11-18 12:25:26 -0600

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urtext::Application.config.secret_token = '869dc78da6415a0e76d9d4cc5e954731a6f6500ca6a63143c1df6f2b636422de6b7156278369b6f8160072b8765b7761c06f1743ac1a2c5938174a11816e19db'
