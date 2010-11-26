955b36a800ba25d7d128368abcaeeb1bfc8bbf6c
Kyle Smyth <kylesmyth@gmail.com>
2010-11-26 17:26:34 -0600

    finished ad controller

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,23 +1,49 @@
 class AdsController < ApplicationController
+  respond_to :html, :xml
   def index
+    @ads = Find.all
+    respond_with @ads
   end
   
   def show
+    @ad = Ad.find(params[:id])
+    respond_with @ad
+    
   end
   
   def new
+    @ad = Ad.new
+    respond_with @product
   end
   
   def create
+    @ad = Ad.new(params[:ad])
+    if @ad.save
+      cookies[:last_ad_id] = @ad.id
+      flash[:notice] = "Successfuly created ad"
+    end
+    respond_with(@ad)
   end
   
   def edit
+    @ad = Ad.find(params[:id])
+    respond_with(@ad)
   end
   
   def update
+    @ad = Ad.find(params[:id])
+    if @ad.update_attributes(params[:ad])
+      flash[:notice] = "Successfully updated ad"
+    end
+    respond_with(@ad)
   end
   
   def destroy
+    @ad = Ad.find(params[:id])
+    @ad.destroy
+    flash[:notice] = "Successfuly deleted ad"
+    respond_with(@ad)
+    
   end
   
 end
