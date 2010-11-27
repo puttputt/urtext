c0b652702a36411197b10d7016edf367555767c0
Kyle Smyth <kylesmyth@gmail.com>
2010-11-26 19:48:06 -0600

    fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Urtext::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "ad#index"
+  root :to => "ads#index"
 
   # See how all your routes lay out with "rake routes"
 
