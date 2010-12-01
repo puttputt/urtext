ffd60431415c876e1bb4ae51814e6888f90804ed
Kyle Smyth <kylesmyth@gmail.com>
2010-11-30 21:25:26 -0600

    added a few model validators

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,6 @@
 class Ad < ActiveRecord::Base
     belongs_to :textbook
     belongs_to :user
+    validates_presence_of :type, :price, :description
+    validates_numericality_of :price
 end
--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -1,3 +1,5 @@
 class Textbook < ActiveRecord::Base
   has_one :ad
+  validates_presence_of :name, :author, :edition
+  validates_numericality_of :edition, :only_integer => true
 end
